620bf82405ee419a95e496bcaaa9acfe38b45989
edimilson braz <edimilson.gt8@gmail.com>
2023-10-19 14:39:40 -0300

    :sparkles: add ProductItem and ProductList components

--- src/app/(home)/components/category-item.tsx
+++ src/app/(home)/components/category-item.tsx
@@ -1,6 +1,13 @@
 import { Badge } from "@/components/ui/badge";
 import { Category } from "@prisma/client";
-import { Headphones, KeyboardIcon, MonitorIcon, MouseIcon, SpeakerIcon, SquareIcon } from "lucide-react";
+import {
+  Headphones,
+  KeyboardIcon,
+  MonitorIcon,
+  MouseIcon,
+  SpeakerIcon,
+  SquareIcon,
+} from "lucide-react";
 
 interface CategoryItemProps {
   category: Category;
@@ -8,13 +15,13 @@ interface CategoryItemProps {
 
 const CategoryItem = ({ category }: CategoryItemProps) => {
   const categoryIcon = {
-    keyboards: <KeyboardIcon  size={16}/>,
-    monitors: <MonitorIcon  size={16}/>,
-    headphones: <Headphones  size={16}/>,
-    mousepads: <SquareIcon  size={16}/>,
-    speakers: <SpeakerIcon  size={16}/>,
-    mouses: <MouseIcon  size={16}/>,
-  }
+    keyboards: <KeyboardIcon size={16} />,
+    monitors: <MonitorIcon size={16} />,
+    headphones: <Headphones size={16} />,
+    mousepads: <SquareIcon size={16} />,
+    speakers: <SpeakerIcon size={16} />,
+    mouses: <MouseIcon size={16} />,
+  };
   return (
     <Badge
       variant={"outline"}
