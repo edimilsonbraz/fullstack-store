e6c53ce2f6204bcf1665c0ff557ad6607a9e4a73
edimilson braz <edimilson.gt8@gmail.com>
2023-10-18 21:34:13 -0300

    :sparkles: Add banner to home page

---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,16 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="p-5">
+      <Image
+        className="h-auto w-full"
+        sizes="100vw"
+        src="/banner-home-01.png"
+        height={0}
+        width={0}
+        alt="Até 50% de desconto esse mês!"
+      />
+    </div>
+  );
+}
