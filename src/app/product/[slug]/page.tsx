0bd968eff018dcb7218cc529935612bcd65ba5b9
edimilson braz <edimilson.gt8@gmail.com>
2023-10-23 16:32:41 -0300

    :sparkles: ensure recommended products are shown on product details page

--- src/app/product/[slug]/page.tsx
+++ src/app/product/[slug]/page.tsx
@@ -1,29 +1,46 @@
-import { prismaClient } from "@/lib/prisma"
-import ProductImages from "../components/product-images"
-import ProductInfo from "../components/product-info"
-import { computeProductTotalPrice } from "@/helpers/product"
+import { prismaClient } from "@/lib/prisma";
+import ProductImages from "../components/product-images";
+import ProductInfo from "../components/product-info";
+import { computeProductTotalPrice } from "@/helpers/product";
+import ProductList from "@/components/ui/product-list";
 
 interface ProductDetailsPageProps {
   params: {
-    slug: string
-  }
+    slug: string;
+  };
 }
 
-const ProductDetailsPage = async ({params: { slug }}: ProductDetailsPageProps) => {
+const ProductDetailsPage = async ({
+  params: { slug },
+}: ProductDetailsPageProps) => {
   const product = await prismaClient.product.findFirst({
     where: {
-      slug: slug
+      slug: slug,
+    },
+    include: {
+      category: {
+        include: {
+          products: {
+            where: {
+              slug: {
+                not: slug
+              }
+            }
+          },
+        }
+      }
     }
-  })
+  });
 
-  if(!product) return null;
+  if (!product) return null;
 
   return (
-    <div className="flex flex-col gap-8">
+    <div className="flex flex-col gap-8 pb-8">
       <ProductImages imageUrls={product.imageUrls} name={product.name} />
       <ProductInfo product={computeProductTotalPrice(product)} />
+      <ProductList products={product.category.products} />
     </div>
-  )
-}
+  );
+};
 
 export default ProductDetailsPage;
