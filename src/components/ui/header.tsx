327720a0ac2a18d9614194b64ebc1df8aa43ae24
edimilson braz <edimilson.gt8@gmail.com>
2023-10-27 14:51:38 -0300

    :sparkles: update cart and footer

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -136,7 +136,7 @@ const Header = () => {
           </Button>
         </SheetTrigger>
 
-        <SheetContent>
+        <SheetContent className="w-[380px]">
           <Cart />
         </SheetContent>
       </Sheet>
