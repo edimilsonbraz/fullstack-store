f83558ba5591f37c4607fca3ce37895022187787
edimilson braz <edimilson.gt8@gmail.com>
2023-10-30 08:40:55 -0300

    :sparkles: add persistence to cart product

--- src/providers/cart.tsx
+++ src/providers/cart.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { ProductWithTotalPrice } from "@/helpers/product";
-import { ReactNode, createContext, useMemo, useState } from "react";
+import { ReactNode, createContext, useEffect, useMemo, useState } from "react";
 
 export interface CartProduct extends ProductWithTotalPrice {
   quantity: number;
@@ -36,7 +36,14 @@ export const CartContext = createContext<ICartContext>({
 });
 
 const CartProvider = ({ children }: { children: ReactNode }) => {
-  const [products, setProducts] = useState<CartProduct[]>([]);
+  const [products, setProducts] = useState<CartProduct[]>(
+    JSON.parse(localStorage.getItem("@fsw-store/cart") || "[]")
+  );
+
+  useEffect(() => {
+    //Salvando o carrinho no LocalStorage
+    localStorage.setItem("@fsw-store/cart", JSON.stringify(products))
+  }, [products])
 
   //Total sem descontos
   const subtotal = useMemo(() => {
