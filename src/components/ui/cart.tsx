fca84bf27c0cc127a6546cda1439d8bfb4a38c6a
edimilson braz <edimilson.gt8@gmail.com>
2023-11-02 09:31:58 -0300

    :bug: ensure order is updated on checkout success

--- src/components/ui/cart.tsx
+++ src/components/ui/cart.tsx
@@ -22,10 +22,11 @@ const Cart = () => {
       //TODO: redirecionar para o login 
       return
     }
+    
     //Cria pedido no Banco
-    await createOrder(products, (data?.user as any).id)
+    const order = await createOrder(products, (data?.user as any).id)
 
-    const checkout = await createCheckout(products)
+    const checkout = await createCheckout(products, order.id)
 
     const stripe = await loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY)
 
